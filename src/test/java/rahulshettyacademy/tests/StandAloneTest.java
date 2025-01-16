8c3f7e5acc95fe36165be52c4a2187caf136dd0f
fti-mmaddareddy <mmaddareddy@ftilab.com>
2025-01-16 20:27:08 +0530

    Standloane test

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -1,27 +1,36 @@
 package rahulshettyacademy.tests;
 
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import rahulshettyacademy.TestComponents.BaseTest;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.CheckoutPage;
+import rahulshettyacademy.pageobjects.ConfirmationPage;
+import rahulshettyacademy.pageobjects.ProductCatalogue;
 
 public class StandAloneTest extends BaseTest {
 
 
 
 
-//	@Test
-//	public void submitOrder() throws IOException {
-//		String productName = "ZARA COAT 3";
-//		String countryName = "India";
-//		ProductCatalogue productCatalogue = landingPage.loginApplication("manojreddym18@gmail.com", "107Me13025@");
-//		productCatalogue.addProductToCart(productName);
-//		CartPage cartpage = productCatalogue.goToCartPage();
-//		boolean match = cartpage.verifyProductDisplay(productName);
-//		Assert.assertTrue(match);
-//		CheckoutPage checkoutPage = cartpage.checkout();
-//
-//		checkoutPage.selectCountry(countryName);
-//		ConfirmationPage confirmationPage = checkoutPage.clickPlaceOrderButton();
-//
-//		String confirmMessage = confirmationPage.getConfirmationMessage();
-//		Assert.assertEquals(confirmMessage, "THANKYOU FOR THE ORDER.", "Order confirmation message mismatch");
-//	}
+	@Test
+	public void submitOrder() throws IOException {
+		String productName = "ZARA COAT 3";
+		String countryName = "India";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("manojreddym18@gmail.com", "107Me13025@");
+		productCatalogue.addProductToCart(productName);
+		CartPage cartpage = productCatalogue.goToCartPage();
+		boolean match = cartpage.verifyProductDisplay(productName);
+		Assert.assertTrue(match);
+		CheckoutPage checkoutPage = cartpage.checkout();
+
+		checkoutPage.selectCountry(countryName);
+		ConfirmationPage confirmationPage = checkoutPage.clickPlaceOrderButton();
+
+		String confirmMessage = confirmationPage.getConfirmationMessage();
+		Assert.assertEquals(confirmMessage, "THANKYOU FOR THE ORDER.", "Order confirmation message mismatch");
+	}
 }
